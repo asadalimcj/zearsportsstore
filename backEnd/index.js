10073199c3dddcfe486a8f33f0ea53d1f9c37c41
asadalimcj <120159273+asadalimcj@users.noreply.github.com>
2024-09-09 20:19:59 +0500

    update

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const mongoDB = require("./Db");
 mongoDB();
-app.use((req, res, next) =>{
-    res.setHeader("Access-Control-Allow-Origin", "https://zearsportsstore-frontend.vercel.app/");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-})
+// app.use((req, res, next) =>{
+//     res.setHeader("Access-Control-Allow-Origin", "https://zearsportsstore-frontend.vercel.app/");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept"
+//     );
+//     next();
+// })
+app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://zearsportsstore-frontend.vercel.app'], // Add your frontend URL(s) here
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allowed methods
+    credentials: true,  // Allow sending cookies with CORS requests
+  };
+
+  app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use("/api", require("./Routes/CreateUser"));
