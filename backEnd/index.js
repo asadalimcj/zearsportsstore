fc8a5f23ac031e0d023fbee51e79b98a56415227
asadalimcj <120159273+asadalimcj@users.noreply.github.com>
2024-09-09 20:16:53 +0500

    update

--- backEnd/index.js
+++ backEnd/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoDB = require("./Db");
 mongoDB();
 app.use((req, res, next) =>{
-    res.setHeader("Access-Control-Allow-Origin", "https://zearsportsstore-frontend.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "https://zearsportsstore-frontend.vercel.app/");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
